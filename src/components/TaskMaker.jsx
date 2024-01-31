f329e7dbe3d2ed62042954b4eaf865d493f1a055
Seyam <seyam.hossain.118833@gmail.com>
2024-02-01 02:17:35 +0600

    taskMaker Component completed

--- src/components/TaskMaker.jsx
+++ src/components/TaskMaker.jsx
@@ -1,11 +1,16 @@
+import { DatePicker } from "keep-react";
 import { useState } from "react";
 import styles from "../styles/TaskMaker.module.css";
 
 export default function TaskMaker() {
   const [task, setTask] = useState("");
+  const [startTime, setStartTime] = useState(null);
+  const [endTime, setEndTime] = useState(null);
   const handelSubmit = (e) => {
     e.preventDefault();
     console.log(task);
+    console.log(startTime);
+    console.log(endTime);
   };
   return (
     <div className={styles.task_maker}>
@@ -16,10 +21,24 @@ export default function TaskMaker() {
             className={styles.task_input}
             placeholder="What is the task?"
             onChange={(e) => setTask(e.target.value)}
+            required
           />
           <button type="submit" className={styles.add_task_btn}>
             Add
           </button>
+        </div>
+
+        <div className={styles.time_picker_area}>
+          <DatePicker time={setStartTime}>
+            <span className={styles.time_picker_text}>Select start time</span>
+            <DatePicker.Time />
+          </DatePicker>
+        </div>
+        <div className={styles.time_picker_area}>
+          <DatePicker time={setEndTime}>
+            <span className={styles.time_picker_text}>Select end time</span>
+            <DatePicker.Time />
+          </DatePicker>
         </div>
       </form>
     </div>
