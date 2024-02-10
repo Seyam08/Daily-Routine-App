ae1be628b956b27f0bef1409b0a6d354418fe5b0
Seyam <seyam.hossain.118833@gmail.com>
2024-02-10 23:57:13 +0600

    task displayed

--- src/components/RoutineWrapper.jsx
+++ src/components/RoutineWrapper.jsx
@@ -1,22 +1,44 @@
 import { useState } from "react";
+import uniqid from "uniqid";
 import styles from "../styles/RoutineWrapper.module.css";
 import Task from "./Task";
 import TaskMaker from "./TaskMaker";
 
 export default function RoutineWrapper() {
-  const [tasks, setTasks] = useState({
-    Task: "Let's do something",
-    StartTime: "8:45pm",
-    EndTime: "12:00pm",
-  });
-  console.log(tasks);
-
+  const [tasks, setTasks] = useState([
+    {
+      id: uniqid(),
+      Task: "Go for walk",
+      StartTime: "6:00am",
+      EndTime: "7:00am",
+      completed: false,
+      isEditing: false,
+    },
+  ]);
+  const addTasks = (task) => {
+    setTasks([
+      ...tasks,
+      {
+        id: uniqid(),
+        Task: task.Task,
+        StartTime: task.StartTime,
+        EndTime: task.EndTime,
+        completed: false,
+        isEditing: false,
+      },
+    ]);
+  };
+  // console.log(tasks);
   return (
     <div className={styles.rtn_wrapper}>
       <h2 className={styles.app_heading}>Daily Routine App</h2>
       <div className={styles.rtn_layout}>
-        <TaskMaker setTasks={setTasks} />
-        <Task></Task>
+        <TaskMaker setTasks={addTasks} />
+        {/* {tasks.map((task) => (
+          <Task key={task.id} task={task} />
+          // console.log(task);
+        ))} */}
+        <Task key={tasks.id} tasks={tasks} />
       </div>
     </div>
   );
--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,15 +1,16 @@
 import { Badge, Table } from "keep-react";
 
-export default function TableComponent() {
+export default function Task({ tasks }) {
+  console.log(tasks);
   return (
     <Table showCheckbox={true}>
       <Table.Caption>
         <div className="my-5 flex items-center justify-between px-6">
           <div className="flex items-center gap-5">
-            <p className="text-body-1 font-semibold text-metal-600">Tasks</p>
-            <Badge size="xs" colorType="light" color="gray">
-              100 Member
+            <Badge size="sm" colorType="light" color="gray">
+              {tasks.length}
             </Badge>
+            <p className="text-body-1 font-semibold text-metal-600">Tasks</p>
           </div>
         </div>
       </Table.Caption>
@@ -21,27 +22,29 @@ export default function TableComponent() {
         <Table.HeadCell>End to</Table.HeadCell>
       </Table.Head>
       <Table.Body className="divide-gray-25 divide-y">
-        <Table.Row className="bg-white">
-          <Table.Cell>
-            <div className="flex items-center gap-3">
-              <div className="flex items-center gap-4">
-                <div className="flex items-center gap-2">
-                  <div>
-                    <p className="-mb-0.5 text-body-4 font-medium text-metal-600">
-                      Lets do something
-                    </p>
+        {tasks.map((task) => (
+          <Table.Row className="bg-white" key={task.id}>
+            <Table.Cell>
+              <div className="flex items-center gap-3">
+                <div className="flex items-center gap-4">
+                  <div className="flex items-center gap-2">
+                    <div>
+                      <p className="-mb-0.5 text-body-4 font-medium text-metal-600">
+                        {task.Task}
+                      </p>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          </Table.Cell>
-          <Table.Cell>
-            <p className="text-cyan-600 font-bold ">6:00pm</p>
-          </Table.Cell>
-          <Table.Cell>
-            <p className="text-cyan-600 font-bold">6:00pm</p>
-          </Table.Cell>
-        </Table.Row>
+            </Table.Cell>
+            <Table.Cell>
+              <p className="text-cyan-600 font-bold ">{task.StartTime}</p>
+            </Table.Cell>
+            <Table.Cell>
+              <p className="text-cyan-600 font-bold">{task.EndTime}</p>
+            </Table.Cell>
+          </Table.Row>
+        ))}
       </Table.Body>
     </Table>
   );
